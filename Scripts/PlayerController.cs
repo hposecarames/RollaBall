60475e9786c429559d91a6249db84df2e821cec2
Hector <hposecarames@danielcastelao.org>
2019-10-29 11:20:47 +0100

    RollaBall finalizado

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+   public float speed;
+   public Text countText;
+   public Text winText;
+   private Rigidbody jugador;
+   private int count;
+
+   void Start() {
+      jugador = GetComponent<Rigidbody>();
+      count = 0;
+      SetCountText();
+      winText.text = "";
+   }
+   void FixedUpdate() {
+      
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical");
+      Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+      jugador.AddForce (movement * speed);
+   }
+   void OnTriggerEnter(Collider other) {
+
+      if(other.gameObject.CompareTag("Pick Up")){
+         other.gameObject.SetActive(false);
+         count = count +10;
+         SetCountText();
+
+      }  
+      if (other.gameObject.tag == "Enemigo")
+        {
+            countText.text = "Ouch!!!!!";
+            count = count - 3;
+            // vuelve al centro del tablero
+            jugador.transform.position = Vector3.zero;
+        }
+   }
+   void SetCountText(){
+      
+      countText.text = "Count: " + count.ToString();
+      if (count >=100){
+         winText.text = "YOU WIN!!!";
+      }
+      if (count > 90 && count < 100){
+         winText.text = "YOU LOSE!!!";
+      }
+
+
+   }
+}
---
+++ Scripts/SeguirJugador.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirJugador : MonoBehaviour
+{
+    public Transform jugador;
+    UnityEngine.AI.NavMeshAgent enemigo;
+    private bool dentro = false;
+    Animator animator;
+    public float proximidad = 4f;
+
+    void Start()
+    {
+        enemigo = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = jugador.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dentro = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dentro = false;
+        }
+    }
+
+    void Update()
+    {
+
+        if (!dentro)
+        {
+            enemigo.destination = jugador.position;
+        }
+        if (dentro)
+        {
+            enemigo.destination = this.transform.position;
+        }
+
+        
+       //Comparamos la distancia del enemigo para cambiar el estado del animator, por defecto es "Tranquilo"
+        if (!enemigo.pathPending && (enemigo.remainingDistance < proximidad))
+        {
+            //Si la distancia es menor de 4f se activa el estado y la animación de peligro
+            Debug.Log("Peligro");
+            animator.SetBool("Peligro", true);
+            
+        }
+        else
+        {
+            //Si la distancia es mayor de 4f se desactiva peligro y se activa la animación "Tranquilo"
+            Debug.Log("Tranquilo");
+            animator.SetBool("Peligro", false);
+            
+
+
+        }
+    }
+}
